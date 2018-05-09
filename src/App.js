bba9c4c28467d9d0c2be2d63b97f472c1e409e86
Ali Alizada <akbar.alizadeh5@gmail.com>
2018-05-09 11:40:40 -0700

    error handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import CatchError from './Error'
 import Root from './router/Root';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <CatchError>
         <Root />
-      </div>
+      </CatchError>
     );
   }
 }
